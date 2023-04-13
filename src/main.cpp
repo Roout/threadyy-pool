ea5bfe346e9a3534fbd9d848c55fb7465f81c081
Roout <following2future@gmail.com>
2023-04-13 16:06:50 +0300

    Invoke future result

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,24 @@
 int main() {
     klyaksa::ThreadPool pool {2};
     std::atomic<int> counter;
+    std::atomic<bool> stopped { false };
+    auto console = pool.Post([&](){
+        int iters = 0;
+        while (!stopped.load(std::memory_order_acquire)) {
+            std::cout << "* ";
+            std::this_thread::sleep_for(std::chrono::milliseconds{100});
+            iters++;
+        }
+        return iters;
+    }).value();
     for (size_t i = 0; i < 10; i++) {
         (void) pool.Post([&](){ counter++; });
     }
     pool.Start();
     std::this_thread::sleep_for(std::chrono::seconds{2});
+    stopped.store(true, std::memory_order_release);
     pool.Stop();
+    std::cout << "\nastrics: " << console.get() << '\n';
     std::cout << counter << '\n';
     return 0;
 }
