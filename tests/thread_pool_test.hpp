a6152aee93a1adf8d874710a3a3329c96cdc9bc7
Roout <following2future@gmail.com>
2023-05-21 11:30:09 +0300

    Add tests for compiling with different callable

--- tests/thread_pool_test.hpp
+++ tests/thread_pool_test.hpp
@@ -38,6 +38,118 @@ TEST(thread_pool, executor_lifetime_cycle) {
     ASSERT_EQ(executor.GetActiveTasks(), 0);
 }
 
+TEST(thread_pool, post_lambdas) {
+    using namespace std::chrono_literals;
+
+    static constexpr std::size_t kWorkers{5};
+    klyaksa::ThreadPool executor{kWorkers};
+    executor.Start();
+    std::atomic<int> count{0};
+    auto results = { executor.Post([] { std::this_thread::sleep_for(5ms);})
+        , executor.Post([&count] { count++; std::this_thread::sleep_for(5ms);})
+        , executor.Post([&count](int x) { count += x; std::this_thread::sleep_for(5ms);}, 1)
+        , executor.Post([&count](int x) noexcept { count += x; std::this_thread::sleep_for(5ms);}, 1)
+        , executor.Post([&count](int x, int y) { count += x + y; std::this_thread::sleep_for(5ms);}, 1, 2)
+        , executor.Post([&count](int x, int y) mutable { count += x + y; std::this_thread::sleep_for(5ms);}, 1, 2)
+    };
+    auto res = executor.Post([&count] { count++; std::this_thread::sleep_for(5ms); return count.load(); });
+    std::this_thread::sleep_for(50ms);
+    executor.Stop();
+}
+
+namespace {
+    void EmptyTest() {}
+    void WithParam(std::atomic<int> &x) { x++; }
+    int WithReturn(int x) { return x + 1; }
+    int WithConstParam(const int& x) { return x * x; }
+    int WithRvalueParam(int&&x) { x++; auto y = std::move(x); return y + 1; }
+} // namespace {
+
+TEST(thread_pool, post_raw_functions) {
+    using namespace std::chrono_literals;
+
+    static constexpr std::size_t kWorkers{5};
+    klyaksa::ThreadPool executor{kWorkers};
+    executor.Start();
+    std::atomic<int> inc { 0 };
+    int noChange { 0 };
+    (void) executor.Post(EmptyTest);
+    (void) executor.Post(&EmptyTest);
+    (void) executor.Post(WithParam, std::ref(inc));
+    (void) executor.Post(&WithParam, std::ref(inc));
+    (void) executor.Post(WithReturn, noChange);
+    (void) executor.Post(&WithConstParam, std::cref(noChange));
+    (void) executor.Post(WithConstParam, std::cref(noChange));
+    (void) executor.Post(&WithRvalueParam, 1);
+    (void) executor.Post(WithRvalueParam, 2);
+    
+    auto optFuture = executor.Post(&WithReturn, noChange);
+    std::this_thread::sleep_for(50ms);
+    EXPECT_TRUE(optFuture);
+    EXPECT_EQ(optFuture->get(), 1);
+    EXPECT_EQ(inc, 2);
+    executor.Stop();
+}
+
+TEST(thread_pool, task_throw_exception) {
+
+}
+
+TEST(thread_pool, post_functor) {
+    using namespace std::chrono_literals;
+    struct Empty {
+        void operator()() {}
+    };
+    struct EmptyConst {
+        void operator()() const noexcept {}
+    };
+    struct WithParam {
+        void operator()(int &x) const noexcept { ++x; }
+    };
+    struct WithCapture {
+        WithCapture(int x) : x_ { x } {}
+        void operator()() noexcept { x_++; }
+        int x_;
+    };
+    struct WithReturn {
+        WithReturn(int x) : x_ { x } {}
+        int operator()() noexcept { x_++; return x_; }
+        int x_;
+    };
+    static constexpr std::size_t kWorkers{5};
+
+    klyaksa::ThreadPool executor{kWorkers};
+    executor.Start();
+    int x = 0; 
+    // as rvalue
+    (void) executor.Post(Empty{});
+    (void) executor.Post(EmptyConst{});
+    (void) executor.Post(WithParam{}, x);
+    (void) executor.Post(WithCapture{1});
+    (void) executor.Post(WithReturn{1});
+    (void) executor.Post(std::function<void()>{[x]{}});
+    (void) executor.Post(std::move_only_function<void()>{[x]{}});
+    
+    // as lvalue
+    Empty empty{};
+    EmptyConst emptyConst{};
+    WithParam withParams{};
+    WithCapture withCapture{1};
+    WithReturn withReturn{1};
+    std::function<void()> func{[x]{}};
+    std::move_only_function<void()> moveFunc{[x]{}};
+    (void) executor.Post(empty);
+    (void) executor.Post(emptyConst);
+    (void) executor.Post(withParams, x);
+    (void) executor.Post(withCapture);
+    (void) executor.Post(withReturn);
+    (void) executor.Post(func);
+    (void) executor.Post(std::move(moveFunc));
+    std::this_thread::sleep_for(50ms);
+    executor.Stop();
+}
+
+
 TEST(thread_pool, access_future_from_post_result) {
     using namespace std::chrono_literals;
 
